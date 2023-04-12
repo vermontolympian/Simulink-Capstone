ececcdd43aad07e95c9912fcf6459dc91d72879b
Brandon <2boog25@gmail.com>
2023-04-12 15:54:20 -0400

    Getting somewhere

--- TorqueOnHardwareSingle.m
+++ TorqueOnHardwareSingle.m
@@ -83,25 +83,25 @@ dxl_present_position = 0;                   % Present position
 
 
 % Open port
-% if (openPort(port_num))
-%     fprintf('Succeeded to open the port!\n');
-% else
-%     unloadlibrary(lib_name);
-%     fprintf('Failed to open the port!\n');
-%     input('Press any key to terminate...\n');
-%     return;
-% end
-% 
-% 
-% % Set port baudrate
-% if (setBaudRate(port_num, BAUDRATE))
-%     fprintf('Succeeded to change the baudrate!\n');
-% else
-%     unloadlibrary(lib_name);
-%     fprintf('Failed to change the baudrate!\n');
-%     input('Press any key to terminate...\n');
-%     return;
-% end
+if (openPort(port_num))
+    fprintf('Succeeded to open the port!\n');
+else
+    unloadlibrary(lib_name);
+    fprintf('Failed to open the port!\n');
+    input('Press any key to terminate...\n');
+    return;
+end
+
+
+% Set port baudrate
+if (setBaudRate(port_num, BAUDRATE))
+    fprintf('Succeeded to change the baudrate!\n');
+else
+    unloadlibrary(lib_name);
+    fprintf('Failed to change the baudrate!\n');
+    input('Press any key to terminate...\n');
+    return;
+end
 
 
 % Enable Dynamixel Torque
