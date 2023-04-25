bfe91d67b784d858997f187474804f6433cdaf58
Brandon <2boog25@gmail.com>
2023-04-25 19:46:14 -0400

    Getting somewhere

---
+++ init_release_script.m
@@ -0,0 +1,24 @@
+%%
+loadlibrary('dxl_x64_c', 'dynamixel_sdk.h', 'addheader', 'port_handler.h', 'addheader', 'packet_handler.h');
+
+Ts = 0.02;
+myDxl = slDxl('COM6', 1000000);
+myDxl.findDxls()
+myDxl
+
+%%
+myDxl.doEnableTorque(11);
+myDxl.doEnableTorque(12);
+myDxl.doEnableTorque(13);
+myDxl.doEnableTorque(14);
+myDxl.doEnableTorque(15);
+
+%%
+myDxl.doDisableTorque(11);
+myDxl.doDisableTorque(12);
+myDxl.doDisableTorque(13);
+myDxl.doDisableTorque(14);
+myDxl.doDisableTorque(15);
+
+%%
+myDxl.delete()
